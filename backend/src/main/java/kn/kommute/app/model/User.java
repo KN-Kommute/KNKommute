1bcd4ad9ba89682a069d6765b35c99b8ec230a35
DiogoSilva78 <diogosousasilva07@gmail.com>
2025-05-06 13:33:44 +0100

    Creation of files
    Creation of the following files:
    
    Model: User.java
    Repository: UserRepository.java

---
+++ backend/src/main/java/kn/kommute/app/model/User.java
@@ -0,0 +1,29 @@
+package kn.kommute.app.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 40)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "phone_number", unique = true, nullable = false, length = 20)
+    private String phoneNumber;
+}
