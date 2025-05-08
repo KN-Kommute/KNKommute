58131bb783e69521efe88f802d585b0435b37710
Diogo Silva <73041518+DiogoSilva78@users.noreply.github.com>
2025-05-08 10:17:38 +0100

    Creation of the User ( class/repository ) (#3)
    * Tables Creation
    
    We created two tables named: Users and rides
    
    * Fix table creation
    
    * Creation of files
    
    Creation of the following files:
    
    Model: User.java
    Repository: UserRepository.java
    
    * Tables Creation
    
    We created two tables named: Users and rides
    
    * Fix table creation
    
    * Creation of files
    
    Creation of the following files:
    
    Model: User.java
    Repository: UserRepository.java
    
    * Fix user.java file
    
    Fix user.java file with the changes requested on the PR

---
+++ backend/src/main/java/kn/kommute/app/model/User.java
@@ -0,0 +1,31 @@
+package kn.kommute.app.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 40)
+    private String name;
+
+    @Email
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    @Column(name = "phone_number", unique = true, nullable = false, length = 20)
+    private String phoneNumber;
+}
