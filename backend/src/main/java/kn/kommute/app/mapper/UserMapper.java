374d9be7a752ed85f15c00d2f8a3ffbf8e1f5dc4
jmoura-a <joaomiguelcostamoura@gmail.com>
2025-05-22 11:48:51 +0100

    user_controller correctiion #3
    add: dtos change: service, mapper and controller

--- backend/src/main/java/kn/kommute/app/mapper/UserMapper.java
+++ backend/src/main/java/kn/kommute/app/mapper/UserMapper.java
@@ -6,8 +6,6 @@ import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
-
     UserDTO toDTO(User user);
-
-    User toEntity(UserDTO userDTO);
+    User toEntity(UserDTO dto);
 }
