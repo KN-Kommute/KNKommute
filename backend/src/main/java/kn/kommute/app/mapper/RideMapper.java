d3674c3568386355a9fb335826d3f9ca00d65f02
DiogoSilva78 <diogosousasilva07@gmail.com>
2025-05-27 11:47:05 +0100

    fix RideMapper interface

--- backend/src/main/java/kn/kommute/app/mapper/RideMapper.java
+++ backend/src/main/java/kn/kommute/app/mapper/RideMapper.java
@@ -3,7 +3,9 @@ package kn.kommute.app.mapper;
 import kn.kommute.app.dto.RideDTO;
 import kn.kommute.app.model.Ride;
 import kn.kommute.app.model.User;
+import org.mapstruct.Mapper;
 
+@Mapper(componentModel = "spring")
 public interface RideMapper {
     Ride toRide(RideDTO dto, User user);
     RideDTO toRideDTO(Ride ride);
